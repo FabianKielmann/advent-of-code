076c281242996ec6373e0239a9002250b783ef21
Fabian <fabian.kielmann@gmx.de>
2021-12-06 21:01:59 +0100

    Recoded solution because for part 2, the first approach ended in a heap overflow. Part 1 and 2 now in one file.

--- day_6/day_6.js
+++ day_6/day_6.js
@@ -1,29 +1,51 @@
 const fs = require('fs')
-const puzzle_input = fs.readFileSync(__dirname + '/input.txt', 'utf8')
+const input = fs.readFileSync(`${__dirname}/input.txt`, 'utf8')
 
-let data = puzzle_input.split(',').map(el => parseInt(el))
+const data = input.split(',').map(num => parseInt(num))
 
-let DAYS = 80
+function solvePuzzle(part = 1) {
+	// Filter array to determine amount of fishes with value "val"
+	let fishes = []
+	fishes.push(data.filter(val => val === 0).length)
+	fishes.push(data.filter(val => val === 1).length)
+	fishes.push(data.filter(val => val === 2).length)
+	fishes.push(data.filter(val => val === 3).length)
+	fishes.push(data.filter(val => val === 4).length)
+	fishes.push(data.filter(val => val === 5).length)
+	fishes.push(data.filter(val => val === 6).length)
+	fishes.push(data.filter(val => val === 7).length)
+	fishes.push(data.filter(val => val === 8).length)
 
-for (let i = 0; i < DAYS; i++) {
-  let newborns = []
+	let DAYS
 
-  data.forEach((fish, fishIdx) => {
-    switch (fish) {
-      case 0:
-        data[fishIdx] = 6
-        newborns.push(8)
-        break
-      default:
-        data[fishIdx]--
-        break
-    }
-  })
+	switch (part) {
+		case 2:
+			DAYS = 256
+			break
+	
+		default:
+			DAYS = 80
+			break
+	}
 
-  // Append newborns
-  data = [...data, ...newborns]
-  console.log(`After ${i<9 ? ' ' : ''}${i+1} day${i===0 ? ' ' : 's'}: \x1b[33m${data.length}\x1b[0m fish.`)
+	// Loop through
+	for (let i = 0; i < DAYS; i++) {
+		let fishPos1 = fishes[0]
+		fishes[0] = fishes[1]
+		fishes[1] = fishes[2]
+		fishes[2] = fishes[3]
+		fishes[3] = fishes[4]
+		fishes[4] = fishes[5]
+		fishes[5] = fishes[6]
+		fishes[6] = fishes[7] + fishPos1
+		fishes[7] = fishes[8]
+		fishes[8] = fishPos1
+	}
+
+	let result = Object.values(fishes).reduce((a, b) => a + b)
+
+	console.log(`Part ${part}: After \x1b[33m${DAYS}\x1b[0m days, there are \x1b[33m${result}\x1b[0m lanternfish.`)
 }
 
-console.log('===')
-console.log(`After ${DAYS} days there are \x1b[32m${data.length}\x1b[0m lanternfish.`)
+solvePuzzle(1)
+solvePuzzle(2)
