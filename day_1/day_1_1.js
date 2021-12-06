f61f310e3eb1af227742b90077115f1b415da1ce
Fabian <fabian.kielmann@gmx.de>
2021-12-06 20:55:08 +0100

    Initial commit

---
+++ day_1/day_1_1.js
@@ -0,0 +1,12 @@
+const fs = require('fs')
+const input = fs.readFileSync(`${__dirname}/input.txt`, 'utf8')
+
+const data = input.split('\r\n').map(num => parseInt(num))
+
+let count = 0
+
+for (let i = 1; i < data.length; i++) {
+  if (data[i] > data[i - 1]) count++
+}
+
+console.log(`Depth measurements increased \x1b[33m${count}\x1b[0m times.`)
---
+++ day_1/day_1_2.js
@@ -0,0 +1,17 @@
+const fs = require('fs')
+const input = fs.readFileSync(`${__dirname}/input.txt`, 'utf8')
+
+const data = input.split('\r\n').map(num => parseInt(num))
+
+let count = 0
+
+for (let i = 0; i < data.length; i++) {
+  if (i === data.length -3) break
+
+  let window1 = data.slice(i, i + 3).reduce((a, b) => a + b)
+  let window2 = data.slice(i + 1, i + 4).reduce((a, b) => a + b)
+
+  if (window2 > window1) count++
+}
+
+console.log(`Depth measurement windows of 3 increased \x1b[33m${count}\x1b[0m times.`)
