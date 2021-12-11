38548d4655f3aae202cc425ffab2874f42189fe0
Fabian <fabian.kielmann@gmx.de>
2021-12-12 00:14:40 +0100

    I've made progress, but it's still WIP

--- day_9/day_9_1.js
+++ day_9/day_9_1.js
@@ -1,24 +1,48 @@
 const fs = require('fs')
 const input = fs.readFileSync(`${__dirname}/input.txt`, 'utf8')
 
-const data = input.split('\n').map(line => line.split('').map(num => parseInt(num)))
+const data = input.split('\r\n').map(line => line.split('').map(num => parseInt(num)))
 
-let valids = []
+const lowestLocations = []
+const basins = []
 
 data.forEach((row, y) => {
   row.forEach((col, x) => {
-    const current = data[y][x]
-    const [top, right, bottom, left] = getSurroundings({ x, y })
-
-    const lowerThanCurrent = [top, right, bottom, left].filter(el => el <= current)
-    if (lowerThanCurrent.length === 0) valids.push(current)
+    const currentValue = data[y][x]
+    const lowerThanCurrent = getSurroundings({ x, y }).filter(el => el.value <= currentValue)
+    if (lowerThanCurrent.length === 0) lowestLocations.push({ x, y, value: currentValue })
   })
 })
 
 // .map with el + 1 because the risk level is height + 1. Then add all together.
-const riskLevelsSum = valids.map(el => el + 1).reduce((prev, next) => next + prev)
+const riskLevelsSum = lowestLocations.map(el => el.value + 1).reduce((prev, next) => next + prev)
 
-console.log(`The risk level sum of all low points is \x1b[33m${riskLevelsSum}\x1b[0m.`)
+console.log(`Part 1: The risk level sum of all low points is \x1b[33m${riskLevelsSum}\x1b[0m.`)
+
+lowestLocations.forEach(lowestLocation => {
+  let basinCount = 1
+  const locationsQueue = [lowestLocation]
+  const alreadyChecked = []
+
+  while (locationsQueue.length > 0) {
+    let current = locationsQueue[0]; // Set current element to the first element in queue
+    alreadyChecked.push(`${current.x},${current.y}`) // Add it to the alreadyChecked array
+    const surroundings = getSurroundings(current) // Get it's surroundings
+    locationsQueue.splice(0, 1) // Remove it from the queue
+    
+    // Check all surroundings of current location
+    surroundings.forEach(el => {
+      if (!alreadyChecked.includes(`${el.x},${el.y}`) && el.value < 9) {
+        basinCount++ // Increase the basin count
+        locationsQueue.push(el) // Push it to the locationsQueue so it's surroundings can be checked next
+      }
+    })
+  }
+
+  basins.push(basinCount)
+})
+
+console.table(basins);
 
 function getSurroundings(current) {
   const top = { x: current.x + 0, y: current.y + -1 }
@@ -28,10 +52,31 @@ function getSurroundings(current) {
 
   let arr = Array(4)
 
-  if (top.y >= 0)               arr[0] = data[top.y][top.x]
-  if (right.x < data[0].length) arr[1] = data[right.y][right.x]
-  if (bottom.y < data.length)   arr[2] = data[bottom.y][bottom.x]
-  if (left.x >= 0)              arr[3] = data[left.y][left.x]
+  if (top.y >= 0)               arr[0] = { ...top, value: data[top.y][top.x] }
+  if (right.x < data[0].length) arr[1] = { ...right, value: data[right.y][right.x] }
+  if (bottom.y < data.length)   arr[2] = { ...bottom, value: data[bottom.y][bottom.x] }
+  if (left.x >= 0)              arr[3] = { ...left, value: data[left.y][left.x] }
 
   return arr
+}
+
+// function getTop(current) {
+//   if (current.y == 0) return false
+
+//   const x = current.x + 0
+//   const y = current.y + -1
+
+//   return { x, y, value: data[y][x] }
+// }
+
+function copyData() { return data.map(arr => arr.slice()) }
+
+function showBasin(basin) {
+  const dataCopy = copyData()
+
+  basin.forEach(el => {
+    dataCopy[el.y][el.x] = 'X'
+  })
+
+  console.table(dataCopy)
 }
